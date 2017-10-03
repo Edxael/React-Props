48c34ca8fe96838a7586f850c5d0b1ba7917ef2c
Edxael <zerphyr.r@gmail.com>
2017-10-03 15:31:36 -0600

    style component

--- src/comps/Profile.jsx
+++ src/comps/Profile.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 class Singer extends React.Component {
   render() {
-    const conSty = { border: "2px solid black", padding: "3px" }
-    const imgSty ={ border: "2px solid black", width: "170", height: "210px"}
+    const conSty = { border: "2px solid black", padding: "3px", width: "180px", background: "gray", margin: "4px" }
+    const imgSty ={ border: "2px solid black", width: "170", height: "210px", margin: "4px auto", display: "block"}
     return(
       <div style={conSty}>
         <img style={imgSty} src={this.props.pic} alt="Missing Pic"/>
--- src/index.js
+++ src/index.js
@@ -2,22 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import ayuPic from './comps/img/ayu.jpg';
+import boraPic from './comps/img/bora.jpg';
+import rosePic from './comps/img/rose.jpg';
+import madPic from './comps/img/mad.jpg';
+import haniPic from './comps/img/hani.jpg';
+import taePic from './comps/img/tae.jpg';
+import hyoPic from './comps/img/hyo.jpg';
+import haruPic from './comps/img/haru.jpg';
 
-// import Meg from './comps/Comp1.jsx';
-// import Cmp2 from './comps/Comp2.jsx';
-// import Cmp3 from './comps/Comp3.jsx';
+
 
 import Profile from './comps/Profile.jsx';
 
 const MainGatherComp = () => {
+  const mainCont = { padding: "20px", background: "black" }
+  const titleSty = { textAlign: "center", background:"rgb(115, 234, 255)", padding: "10px" }
+  const profsCont = { display: "flex", flexWrap: "wrap", background:"rgb(115, 234, 255)", padding: "10px" }
 
   return(
-    <div className="MainST">
-      <h3>Singers Profiles</h3>
+    <div style={mainCont}>
+      <h1 style={titleSty}>Singers Profiles</h1>
 
-      <Profile name= "Ayumi Hamasaki" group= "solo" country= "Japan" pic={ayuPic} />
-
-
+      <div style={profsCont}>
+        <Profile name= "Ayumi Hamasaki" group= "Solist" country= "Japan" pic={ayuPic} />
+        <Profile name= "Bora Sis." group= "Sistar" country= "S. Korea" pic={boraPic} />
+        <Profile name= "Rose Park" group= "Black Pink" country= "S. Korea" pic={rosePic} />
+        <Profile name= "Mad Clown" group= "Solist" country= "S. Korea" pic={madPic} />
+        <Profile name= "Hani" group= "Exid" country= "S. Korea" pic={haniPic} />
+        <Profile name= "Taeyang" group= "Big-Bang" country= "S. Korea" pic={taePic} />
+        <Profile name= "Hyolyn" group= "Sistar" country= "S. Korea" pic={hyoPic} />
+        <Profile name= "Haruka Kodoma" group= "HTK-48" country= "Japan" pic={haruPic} />
+      </div>
 
     </div>
   )
@@ -26,6 +41,10 @@ const MainGatherComp = () => {
 
 ReactDOM.render(<MainGatherComp />, document.getElementById("root"));
 
+
+// import Meg from './comps/Comp1.jsx';
+// import Cmp2 from './comps/Comp2.jsx';
+// import Cmp3 from './comps/Comp3.jsx';
 
 // <Meg />
 //
